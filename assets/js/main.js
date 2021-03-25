aa5f461d8b000c884a33743a649764d85f32a471
Andrea Marangon <marangonandrea84@gmail.com>
2021-03-25 15:34:14 +0100

    primo push con struttura base

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+  el: '#root',
+  data: {
+    todos: [
+      {
+        title: 'Fare la spesa'
+      },
+      {
+        title: 'Andare in piscina'
+      },
+      {
+        title: 'Guardare il webinar'
+      },
+    ],
+  },
+  methods: {
+  }
+});
