18eab738a6586d61982661054dc5df3bca68cb03
Andrea Marangon <marangonandrea84@gmail.com>
2021-03-25 17:14:59 +0100

    aggiunta di check e delete fatto in mattinata

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,47 @@
 var app = new Vue({
   el: '#root',
   data: {
+    inputTxt : '',
     todos: [
       {
-        title: 'Fare la spesa'
+        title: 'Fare la spesa',
+        status: 'todo'
       },
       {
-        title: 'Andare in piscina'
+        title: 'Andare in piscina',
+        status: 'todo'
       },
       {
-        title: 'Guardare il webinar'
+        title: 'Guardare il webinar',
+        status: 'done'
       },
     ],
   },
+  computed : {
+    todosComputed: function () {
+      let todosDone = this.todos.filter((todo) => todo.status == 'done');
+      let todosTodo = this.todos.filter((todo) => todo.status == 'todo');
+      return [...todosTodo, ...todosDone];
+    }
+  },
   methods: {
+    add: function () {
+      if (this.inputTxt != '') {
+        let obj = {
+          title: this.inputTxt,
+          status: 'todo'
+        }
+        this.todos.push(obj);
+        this.inputTxt = '';
+      }
+    },
+    check: function (todo) {
+      let index = this.todos.indexOf(todo);
+      this.todos[index].status = 'done';
+    },
+    remove: function (todo) {
+      let index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    }
   }
 });
